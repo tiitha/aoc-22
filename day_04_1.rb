1eaa44a97c240f078f46cde5d411b5aab1b93dc5
Tiit Hallas <tiit.hallas@dynatrace.com>
2022-12-04 07:43:52 +0200

    day 4

---
+++ day_04_1.rb
@@ -0,0 +1,23 @@
+
+data = '''2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8'''
+
+data = File.open("inp_4").read()
+
+part_1 = 0 
+part_2 = 0 
+
+data = data.split("\n").each do |l|
+	a,b = l.split(",").map { |a| x = a.split("-"); (x[0]..x[1]).to_a }
+	x = a & b 
+
+	part_1 += 1 if a == x or b == x
+	part_2 += 1 if x.size > 0
+end
+
+puts "part 1: #{part_1}"
+puts "part 2: #{part_2}"
