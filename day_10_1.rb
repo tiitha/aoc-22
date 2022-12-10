241fec5518ee0e3444d55cfb83ce79711a28776d
Tiit Hallas <tiit.hallas@dynatrace.com>
2022-12-10 11:06:34 +0200

    day 10

---
+++ day_10_1.rb
@@ -0,0 +1,169 @@
+data = '''addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop'''
+
+data = File.open("inp_10").read()
+
+x = 1
+r = []
+data.split("\n").each do |l|
+	a,b = l.split(" ")
+	r << x
+	next unless a == "addx"
+	r << x
+	x += b.to_i
+end
+
+sum = 0
+res = ""
+(0..r.size-1).each do |c|
+	sum += r[c-1]*c if c == 20 or ((c-20)%40) == 0
+	res += ( (r[c]-1..r[c]+1).include? (c%40) ) ? "#" : "."
+	res += "\n" if (c+1) % 40 == 0
+end
+puts "part 1: #{sum}"
+puts "part 2: \n#{res}"
+
